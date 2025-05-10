18d2a4df8d29b483aeb9fd33c087f6542ab59cd5
sirajshaikh-dev <ss1425306@gmail.com>
2025-05-10 18:22:24 +0530

    feat: add authentication routes and bcrypt dependency for password hashing

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+
+export const register = async (req, res) => {}
+
+export const login = async (req, res) => {}
+
+export const logout = async (req, res) => {}
+
+export const check =async(req, res) =>{}
+
+
+
+
+
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';    
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config(); 
 
 const app = express();
+
+app.use(express.json()); 
+
+app.get('/',(req, res) => {
+    res.send('Welcome to leetlab ');
+});
+
+app.use('/api/v1/auth',authRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port 8080');
