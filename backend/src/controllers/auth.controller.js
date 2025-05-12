8aa0ebc135b459dc1ae5797b487601f77db57177
sirajshaikh-dev <ss1425306@gmail.com>
2025-05-12 12:42:51 +0530

    feat: implement user registration with JWT token generation and error handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,46 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { db } from '../lib/db.js';
+import { UserRole } from '../generated/prisma/index.js';
 
-export const register = async (req, res) => {}
+export const register = async (req, res) => {
+    const {email,password,name}=req.body;
+
+    try {
+        const existingUser = await db.user.findUnique({
+            where:{
+                email
+            }
+        })
+
+        if (existingUser){
+            return res.status(400).json({
+                error:'User already exists'
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await db.user.create({
+            data:{
+                email,
+                password:hashedPassword,
+                name,
+                role:UserRole.USER
+            }
+        })
+
+        const token = jwt.sign(
+            {id:newUser.id},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+        )
+
+    } catch (error) {
+        
+    }
+    
+}
 
 export const login = async (req, res) => {}
 
