6a7bff51de521f304bacad5c7bdda5b40c198035
sirajshaikh-dev <ss1425306@gmail.com>
2025-05-14 22:54:54 +0530

    feat: implement problem management routes and admin check middleware

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,22 @@
+import { db } from "../lib/db.js"
+
+
+
+export const createProblem=()=>{
+    // get all the data from request body
+
+    // check userRole again
+
+    // loop through each refrence solution for different languages.
+
+}
+
+export const getAllProblems=()=>{}
+
+export const getProblemById=()=>{}
+
+export const updateProblem=()=>{}
+
+export const getAllProblemsSolvedByUser=()=>{}
+
+export const deleteProblem=()=>{}
